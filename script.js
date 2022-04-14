81c4556f696c1b6c8fb7e9213b3a153c3a979de8
germanduci <germanducis@gmail.com>
2022-04-13 21:03:29 -0300

    entregable 01

---
+++ script.js
@@ -0,0 +1,12 @@
+let plataforma=false;
+let funca;
+while(!plataforma){
+    funca=(prompt("funciona la plataforma: si/no").toLowerCase());
+        
+        if(funca==="si"){
+            plataforma=true;        
+        }else{    
+            alert("lo siento muchacho, intenta luego")
+        }
+}
+alert("vamo coder!"); 
